8fc98473086a39d287cb3194bd710f773f39611c
Erhan Koyuncu <gerekenbirikim@gmail.com>
2021-11-04 18:22:55 +0300

    Model ve post ekleme dosyaları oluşturuldu, bağlatı ayarları yapıldı

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
-const path = require('path')
-const ejs = require('ejs')
+const mongoose = require('mongoose'); 
+const ejs = require('ejs');
+const Photo = require('./models/Photo')
 
 const app = express();
 app.set("view engine", "ejs");
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 const port = 3000;
 
-app.get('/', (req, res)=>{
- 
-    res.render("index")
+app.get('/', async (req, res)=>{
+
+    const photos =  await Photo.find({} );
+
+    res.render("index", { photos})
 })
 app.get('/about', (req, res)=>{
  
@@ -20,6 +27,13 @@ app.get('/about', (req, res)=>{
 app.get('/add', (req, res)=>{
  
     res.render("add")
+})
+
+app.post('/photos', async (req, res) => {
+ 
+   await Photo.create(req.body);
+
+    res.redirect('/')
 })
 
 
